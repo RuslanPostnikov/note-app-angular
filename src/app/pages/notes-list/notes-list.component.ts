8382bc7ffe645a09bfedd15cd9e6fa2a67dd2d63
RuslanPostnikov <rp02081992@gmail.com>
2022-02-10 13:41:43 +0200

    Searching Notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -82,15 +82,64 @@ export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
 
+  filteredNotes: Note[] = new Array<Note>();
+
   constructor(private noteService: NotesService) { }
 
   ngOnInit(): void {
   //  We want to retrieve all notes from note Service
     this.notes = this.noteService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number) {
     this.noteService.delete(id);
+  }
+
+  filter(query: string) {
+      query = query.toLowerCase().trim();
+
+      let allResults = new Array<Note>();
+      //  split up the search query into individual words
+      let terms: string[] = query.split(' ') // split on spaces
+      //   remove duplicate search terms
+      terms = this.removeDuplicates(terms);
+      //  compile all relevant results into allResults array
+      terms.forEach(term => {
+        let results: Array<Note> = this.relevantNotes(term);
+        //  append results to the AllResults array
+        allResults = [...allResults, ...results];
+      });
+
+      //  allResults will include duplicate notes
+      //  because a particular note can be result of many search terms
+      //  but we don't want to show the same note multiple times on the UI
+      //  so we first must remove duplicates
+      let uniqueResults = this.removeDuplicates(allResults);
+
+      this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicates(arr: Array<any>): Array<any> {
+    let uniqueResults = new Set<any>();
+  //  loop trough the input array and add the items to the set
+    arr.forEach(e => uniqueResults.add(e));
+    return  Array.from(uniqueResults)
+  }
+
+  relevantNotes(query: string): Array<Note> {
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if(note.title && note.title.toLowerCase().includes(query)) return true;
+      if(note.body && note.body.toLowerCase().includes(query)) return true;
+      return false;
+    });
+
+    return relevantNotes;
+  }
+
+  getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
   }
 
 }
